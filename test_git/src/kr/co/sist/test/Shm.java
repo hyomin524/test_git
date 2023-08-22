270736a5c530a469e24e3444cfee2b8c797e7a51
user <user@DESKTOP-COJ9858>
2023-08-22 12:44:04 +0900

    추가 해봄

--- test_git/src/kr/co/sist/test/Shm.java
+++ test_git/src/kr/co/sist/test/Shm.java
@@ -6,6 +6,7 @@ public class Shm {
      System.out.println("이거슨 신기능 추가!!!!!!!");
      System.out.println("대포동 미사일");
      System.out.println("임태균의 미소 !!");
+     System.out.println("추가 테스트");
 	}//main
 
 }//class
