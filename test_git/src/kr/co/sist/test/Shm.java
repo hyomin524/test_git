815584774b41bf8f60830f3050de969b84dc26b8
user <user@DESKTOP-COJ9858>
2023-08-22 12:41:05 +0900

    신기능추가

---
+++ test_git/src/kr/co/sist/test/Shm.java
@@ -0,0 +1,11 @@
+package kr.co.sist.test;
+
+public class Shm {
+
+	public static void main(String[] args) {
+     System.out.println("이거슨 신기능 추가!!!!!!!");
+     System.out.println("대포동 미사일");
+     System.out.println("임태균의 미소!!");
+	}//main
+
+}//class
